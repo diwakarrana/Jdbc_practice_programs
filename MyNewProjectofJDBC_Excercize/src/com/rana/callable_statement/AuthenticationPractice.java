99390b48b5c491ce8a3cfb9268c2e8efdb3dfe69
diwakarrana <diwakarrana464@gmail.com>
2020-04-05 22:28:09 +0530

    I have practice here about how to authenticate with help of underlying procedure into database

--- MyNewProjectofJDBC_Excercize/src/com/rana/callable_statement/AuthenticationPractice.java
+++ MyNewProjectofJDBC_Excercize/src/com/rana/callable_statement/AuthenticationPractice.java
@@ -1,10 +1,28 @@
 package com.rana.callable_statement;
 
+import java.sql.CallableStatement;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.Types;
+import java.util.Scanner;
+
 public class AuthenticationPractice {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	public static void main(String[] args) throws Exception {
+		System.out.println("input username::");
+		Scanner scan = new Scanner(System.in);
+		String user = scan.nextLine();
+		System.out.println("input password::");
+		String pass = scan.nextLine();
+		
+		Class.forName("oracle.jdbc.driver.OracleDriver");
+		Connection con = DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521:orcl", "c##rana","rana123");
+		CallableStatement cs = con.prepareCall("{call auth(?,?,?)}");
+		cs.registerOutParameter(3, Types.VARCHAR);
+		cs.setString(1, user);
+		cs.setString(2, pass);
+		cs.execute();
+	String result = cs.getString(3);
+		System.out.println("this user having  "+result);
 	}
-
 }
